f83a8cf4ec0cd27fe6bcb9739a82acf751828d2b
Ryan S <planetlucid@gmail.com>
2019-10-09 13:32:38 +0000

    show items

--- lib/designer_news/cli.rb
+++ lib/designer_news/cli.rb
@@ -4,6 +4,14 @@ class DesignerNews::CLI
   
   def call
     puts "Today's Designer News:"
+    list_posts
   end
   
+  def list_posts
+    # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
+    puts <<-DOC
+    1. first item title
+    2. second item title
+  DOC
+  end
 end
