209b52c1acf5262ea3c30e784481a7834f15596a
Ryan S <planetlucid@gmail.com>
2019-10-12 00:12:33 +0000

    pulls title and author

--- lib/designer_news/article.rb
+++ lib/designer_news/article.rb
@@ -20,26 +20,26 @@ class DesignerNews::Article
   end
   
   def self.scrape_dn
-    doc = Nokogiri::HTML(open("https://www.designernews.co/"))
-    title = doc.search("a.montana-item-title").first.text  
-    url = doc.search("a.montana-item-title").first
-  end
+      doc = Nokogiri::HTML(open("https://www.designernews.co/"))
+    
+      title = doc.search("a.montana-item-title").first.text
+      name = doc.search("div.montana-item-meta span a").first.text
+      url = 
+      
+      binding.pry
+      end
+    
+    
+      def self.scrape_ux
+      doc = Nokogiri::HTML(open("https://uxdesign.cc"))
+      name =
+      url = 
+      end
 end
 
 
 
-      # article_1 = self.new 
-      # article_1.title = "Article Title 1"
-      # article_1.name = "Article Author 1"
-      # article_1.date = "Article Date 1"
-      # article_1.url = "http://designernews.co"
-      
-      # article_2 = self.new 
-      # article_2.title = "Article Title 2"
-      # article_2.name = "Article Author 2"
-      # article_2.date = "Article Date 2"
-      # article_2.url = "https://uxdesign.cc/"
-      # [article_1, article_2]
+
   
      
 
