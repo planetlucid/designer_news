aa72c47709842eb04426285d4cf2eb5e5ec33779
Ryan S <planetlucid@gmail.com>
2019-10-12 00:58:20 +0000

    prying content from second site

--- lib/designer_news/article.rb
+++ lib/designer_news/article.rb
@@ -1,25 +1,22 @@
 class DesignerNews::Article  
  attr_accessor :title, :name, :date, :url
  
-  def self.today
-    
-    # Scrape Designer News and UxDesign and then return articles based on data
-    # DesignerNews::ArticleScraper.new 
+    def self.today
+      # Scrape Designer News and UxDesign and then return articles based on data
+      # DesignerNews::ArticleScraper.new 
     self.scrape_articles
-  
-  end
+    end
     
-  def self.scrape_articles  
+    def self.scrape_articles  
       articles = []
       articles << self.scrape_dn
-      
-      # Go to Designer News, get article, get properties, show article
-      # Go to UX Design, get article, get properties, show article
+      articles << self.scrape_smash
+   
       
       articles
-  end
+    end
   
-  def self.scrape_dn
+    def self.scrape_dn
       doc = Nokogiri::HTML(open("https://www.designernews.co/"))
     
       article = self.new 
@@ -27,16 +24,27 @@ class DesignerNews::Article
       article.name = doc.search("div.montana-item-meta span a").first.text
       article.url = doc.search("a.montana-item-title").first.attr("href")
       article.date = true 
-      
       article
+    end
+    
+    def self.scrape_smash
+      doc = Nokogiri::HTML(open("https://www.smashingmagazine.com"))
+      binding.pry
+      article = self.new 
+      article.title = doc.search("a.montana-item-title").first.text
+      article.name = doc.search("div.montana-item-meta span a").first.text
+      article.url = doc.search("a.montana-item-title").first.attr("href")
+      article.date = true 
       
+   
+      article
     end
     
   end
-
-
-
-
   
-     
+  
 
+
+
+  # Go to Designer News, get article, get properties, show article
+  # Go to Another Design Site, get article, get properties, show article
